935422e151256aab0a5e780c6357a2c804858218
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2026-01-29 20:37:53 -0400

    layout

--- src/components/navigation/Menu.jsx
+++ src/components/navigation/Menu.jsx
@@ -6,16 +6,18 @@ const Menu = () => {
 
   const linkBase = "block px-4 py-2 rounded-md transition-colors duration-200";
 
-  const linkActive = "bg-slate-900 text-white";
+  const linkActive = "button-menu-active";
 
-  const linkInactive = "text-slate-200 hover:bg-slate-600";
+  const linkInactive = "button-menu-hover";
 
   return (
-    <nav className="bg-slate-700">
+    <nav className="layout-primary">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <span className="text-white font-bold text-lg">Default app</span>
+          <span className="layout-fond-color font-bold text-lg">
+            Default app
+          </span>
 
           {/* Botón mobile */}
           <button
